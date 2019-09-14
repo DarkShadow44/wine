8b487ad8efe7dd1da0abb1c76bb9cc77a893f385
Fabian Maurer <dark.shadow4@web.de>
2019-09-14 16:45:48 +0200

    Move all python into one script

--- tools/make_thunks.py
+++ tools/make_thunks.py
@@ -7,7 +7,131 @@ import clang.cindex
 from clang.cindex import CursorKind
 from pprint import pprint
 from multiprocessing import Pool
+from argparse import ArgumentParser
+from enum import Enum
+import abc
 
+# -------------------- Classes and enums --------------------
+
+class GenericTypeDef(abc.ABC):
+	@abc.abstractmethod
+	def tostring(self) -> str:
+		pass
+
+	@abc.abstractmethod
+	def getname2(self) -> str:
+		pass
+
+class StructDefEnum(Enum):
+	Field = 1
+	Struct = 2
+	Union = 3
+
+class StructDef(GenericTypeDef):
+	def __init__(self, node):
+		self.name = node.spelling
+		self.type = TypeChain(node, node.type)
+		if node.kind == CursorKind.STRUCT_DECL:
+			self.structType = StructDefEnum.Struct
+		if node.kind == CursorKind.UNION_DECL:
+			self.structType = StructDefEnum.Union
+		if node.kind == CursorKind.FIELD_DECL:
+			self.structType = StructDefEnum.Field
+		self.children = []
+		self.named_type = node.type.spelling
+		self.variable = ''
+
+	def print_struct(self, depth):
+		indent = ' ' * 4 * depth
+
+		type = 'error'
+		if self.structType == StructDefEnum.Struct:
+			type = 'struct'
+		if self.structType == StructDefEnum.Union:
+			type  = 'union'
+
+		if self.structType == StructDefEnum.Struct or self.structType == StructDefEnum.Union:
+			print(f'{indent}{type} {self.name}')
+			print(f'{indent}{{')
+			for child in self.children:
+				self.print_struct(child, depth + 1)
+			print(f'{indent}}}{self.variable};')
+			print('')
+
+		if self.structType == StructDefEnum.Field:
+			print(f'{indent}{self.type.tostring(self.name)};')
+
+	def tostring(self):
+		self.print_struct(0);
+
+	def getname(self):
+		return self.name;
+
+class TypeChainEnum(Enum):
+	Array = 1
+	Normal = 2
+	Pointer = 3
+	Function = 4
+
+class TypeChain:
+	def __init__(self, node, type):
+		is_pointer = (type.get_pointee().spelling != '')
+		is_array = (type.get_array_size() != -1)
+		is_func = ('(*)' in type.spelling)
+
+		func_params = []
+		if node:
+			for n in node.get_children():
+				if n.kind == CursorKind.PARM_DECL:
+					func_params.append(TypeChain(None, n.type))
+
+		if is_func:
+			self.chainType = TypeChainEnum.Function
+			self.params = func_params
+			self.result = TypeChain(None, type.get_pointee().get_result())
+		elif is_pointer:
+			self.chainType = TypeChainEnum.Pointer
+			self.subType = TypeChain(None, type.get_pointee())
+		elif is_array:
+			self.chainType = TypeChainEnum.Array
+			self.subType = TypeChain(None, type.get_array_element_type())
+			self.arraySize = type.get_array_size()
+		else:
+			self.chainType = TypeChainEnum.Normal
+			self.normal = type.spelling
+
+	def tostring(self, variable):
+		if self.chainType == TypeChainEnum.Pointer:
+			variable = f' {variable}' if variable != '' else ''
+			return self.subType.tostring('') + f'*{variable}'
+		elif self.chainType == TypeChainEnum.Array:
+			return self.subType.tostring(variable) + f'[{self.arraySize}]'
+		elif self.chainType == TypeChainEnum.Function:
+			paramList = [param.tostring('') for param in self.params]
+			params = ", ".join(paramList) if len(paramList) > 0 else 'void'
+			return f'{self.result.tostring("")} (*{variable}) ({params})';
+		else:
+			variable = f' {variable}' if variable != '' else ''
+			return f'{self.normal}{variable}';
+
+class TypeDef(GenericTypeDef):
+	def __init__(self, source, target):
+		self.source = source
+		self.target = target
+
+	def tostring(self):
+		if self.source.chainType == TypeChainEnum.Function:
+			return f'typedef {self.source.tostring(self.target)};'
+		else:
+			return f'typedef {self.source.tostring("")} {self.target};'
+
+	def getname(self):
+		return target;
+
+
+# -------------------- Helper functions --------------------
+
+# Get all the functions inside a .spec file
 def get_specfile_funcs(path):
 	funcs = []
 	lines = [line.strip() for line in open(path)]
@@ -24,6 +148,7 @@ def get_specfile_funcs(path):
 				funcs.append(rows[2])
 	return funcs
 
+# Get all the sources inside a Makefile.in
 def get_makefile_sources(path):
 	sources = []
 	lines = [line.strip() for line in open(path)]
@@ -112,27 +237,51 @@ def node_is_function_definition(node):
 
 	return False
 
-def find_typerefs(node, ret_nodes, funcs, source):
+def find_all_functions(node, ret_nodes, funcs, source):
 	if (node.location.file is None or not node.location.file.name.endswith(f'/{source}')):
 		return
 	if node.spelling in funcs:
 		if node_is_function_definition(node):
 			ret_nodes.append(node)
 	for c in node.get_children():
-		find_typerefs(c, ret_nodes, funcs, source)
+		find_all_functions(c, ret_nodes, funcs, source)
 
-def handle_dll_source(dll_path, source, funcs, contents_source, ret_func_pointers):
-	path_file = dll_path + "/" + source
+def find_all_structs(node, structdefs_parent):
+	if node.kind == CursorKind.STRUCT_DECL or node.kind == CursorKind.UNION_DECL or node.kind == CursorKind.FIELD_DECL:
+		if ('anonymous union' in node.type.spelling) or ('anonymous struct' in node.type.spelling):
+			for child in structdefs_parent:
+				if child.named_type == node.type.get_named_type().spelling:
+					child.variable = ' ' + node.spelling
+			structdefs_parent = []
+		else:
+			parent = StructDef(node)
+			structdefs_parent.append(parent)
+			structdefs_parent = parent.children
 
+	for c in node.get_children():
+		find_all_structs(c, structdefs_parent)
+
+	if node.kind == CursorKind.TYPEDEF_DECL:
+		type_to = node.spelling
+		type_from = TypeChain(node, node.underlying_typedef_type)
+		structdefs_parent.append(TypeDef(type_from, type_to))
+
+def parse_file(path_file):
 	index = clang.cindex.Index.create()
 	tu = index.parse(path_file,  ["-D_WIN32", "-D__WINESRC__", "-I/usr/lib/clang/8.0.1/include/", "-I../include", "-I/home/fabian/Programming/Wine/wine64/include/", "-fdeclspec", "-Wno-pragma-pack"])
 	if len(tu.diagnostics) > 0:
 		for diag in tu.diagnostics:
 			print(diag.spelling + " " + str(diag.location.file) + ":" + str(diag.location.line))
+	return tu.cursor
+
+def handle_dll_source(dll_path, source, funcs, contents_source, ret_func_pointers):
+	path_file = dll_path + "/" + source
+
+	cursor = parse_file(path_file)
 
 	nodes = []
-	for c in tu.cursor.get_children():
-		find_typerefs(c, nodes, funcs, source)
+	for c in cursor.get_children():
+		find_all_functions(c, nodes, funcs, source)
 
 	# Make function pointers
 	for node in nodes:
@@ -265,5 +414,18 @@ def handle_all_dlls():
 	file_makefile.write('\n'.join(contents_makefile))
 	file_makefile.close()
 
+# -------------------- Entry point --------------------
 
-handle_all_dlls()
+parser = ArgumentParser()
+parser.add_argument('--fixup-sources', dest='fixup_sources', action='store_true', default=False, help='Fixup the wine sources to work with wine-pure. All structures need to have a 32bit layout.')
+parser.add_argument('--generate-thunks-all', dest='generate_thunks_all', action='store_true', default=False, help='Generate all thunks, including the Makefile and shared components.')
+parser.add_argument('--update-thunks', dest='update_thunks', metavar='DLLNAME', help='Regenerate the thunks for a single dll, without touching the Makefile or shared components.')
+
+args = parser.parse_args()
+
+if not args.fixup_sources and not args.generate_thunks_all and not args.update_thunks:
+	print('Please select an option. See -h for help.')
+	exit()
+
+if args.generate_thunks_all:
+	handle_all_dlls()
